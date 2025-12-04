b82dc136ac0d1a8d8f660205b00410173a60b1e1
yisus <yisus@JESUSTRZ>
2025-12-04 00:19:10 -0600

    Agregar archivos de proyecto.

---
+++ Models/DeviceIot.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IotDevicesSimulator.Models
+{
+    public class DeviceIot
+    {
+        // Identificador físico único (simulado)
+        public string MacAddress { get; set; }
+
+        public string Nombre { get; set; }
+        public string Tipo { get; set; } // Ej: "SensorTemperatura", "CamaraSeguridad"
+
+        // Estado actual (Telemetría)
+        public string Status { get; set; } = "Apagado";
+
+        // Aquí guardaremos los ajustes que vienen del Frontend
+        public object Configuracion { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,67 @@
+﻿using IotDevicesSimulator.Models;
+using Microsoft.AspNetCore.SignalR.Client;
+
+// Dispositivo Inicial
+var dispositivo = new DeviceIot
+{
+    MacAddress = "AA:BB:CC:DD:EE:FF", // ID FISICA
+    Nombre = "Sensor de Oficina",
+    Tipo = "SensorTemperatura",
+
+    Configuracion = new
+    {
+        IntervaloEnvio = 5,      // Enviar datos cada 5 segundos
+        Unidad = "Celsius",      // Grados Celsius
+        UmbralAlerta = 30,       // Avisar si pasa de 30 grados
+        ModoAhorro = false       // Modo normal
+    }
+};
+
+Console.WriteLine($"Dispositivo: {dispositivo.Nombre} inicializado.");
+Console.WriteLine($"Configuracion base: {dispositivo.Configuracion}");
+
+    // Conexión
+var connection = new HubConnectionBuilder()
+    .WithUrl($"https://localhost:7291/iotHub?macAddress={dispositivo.MacAddress}") //URL
+    .WithAutomaticReconnect() // Si se cae el internet se reconecta solo
+    .Build();{
+
+    connection.On<object>("RecibirConfiguracion", (nuevaConfig) =>
+    {
+        Console.WriteLine($"[ORDEN RECIBIDA] : {nuevaConfig}");
+    });
+
+    try
+    {
+        await connection.StartAsync();
+        Console.WriteLine("Dispositivo conectado exitosamente!");
+
+        var random = new Random();
+
+        while (true)
+        {
+            var datos = new
+            {
+                Mac = dispositivo.MacAddress,
+                Tipo = "Telemetrica",
+                Valor = random.Next(20, 35),
+                Fecha = DateTime.Now
+            };
+
+            if (connection.State == HubConnectionState.Connected)
+            {
+                await connection.InvokeAsync("ReportarEstado", datos);
+                Console.WriteLine($"[ENVIADO] Temp: {datos.Valor}°C");
+            }
+            dynamic config = dispositivo.Configuracion;
+            int segundos = config.IntervaloEnvio;
+            await Task.Delay(segundos * 1000);
+        }
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Error de conexión: {ex.Message}");
+    }
+
+    Console.ReadLine();
+}
